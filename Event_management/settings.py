2c3e9f5b228983c486e37a278bbc9032967c7c69
Sajid Shahan Rahman <rahmansajid2985@gmail.com>
2026-08-10 22:44:12 +0600

    ke

--- Event_management/settings.py
+++ Event_management/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Event_management.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
